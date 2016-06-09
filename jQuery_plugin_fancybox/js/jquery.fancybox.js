b5bb19c01a70ef5deaae3d7a4c3fcbf8c7252495
Maksym Aleksieiev <cintavra@hotmail.com>
2016-06-09 16:03:15 +0300

    jQuery fancybox plugin

---
+++ jQuery_plugin_fancybox/js/jquery.fancybox.js
@@ -0,0 +1,29 @@
+(function($) {
+    $.fn.fancybox = function(options) {
+
+        var settings = $extend(defaults, options);
+
+        var $link = this;
+        var $body = $('body');
+        var $modal;
+        var $overlay;
+
+        function showModal(e) {
+            var href = $link.attr('href');
+            $modal = $('<div class="fancybox-modal"><img src="' + href + '"></div>');
+            $overlay = $('<div class="fancybox-overlay">');
+        e.preventDefault();
+
+        $body.append($overlay);
+        $body.append($modal);
+        $overlay.one('click', hideModal);
+    }
+        function hideModal() {
+            $modal.hide();
+            $overlay.hide();
+        }
+
+        $link.on('click', showModal);
+        return this;
+    }
+})(jQuery);
